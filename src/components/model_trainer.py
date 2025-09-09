f9c223c8f847f07643145c8c47e1ecadffb17b01
Vishal <vishal.dhasi2004@gmail.com>
2025-09-09 10:53:46 +0530

    Hyperparameter tuning

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -14,7 +14,7 @@ from sklearn.linear_model import LinearRegression
 from src.utils import save_object, evaluate_model
 from sklearn.tree import DecisionTreeRegressor
 from sklearn.neighbors import KNeighborsRegressor
-
+from sklearn.model_selection import GridSearchCV
 @dataclass
 class ModelTrainerConfig:
     trained_model_file_path: str = os.path.join('artifacts', 'model.pkl')
@@ -31,19 +31,41 @@ class ModelTrainer:
             X_test, y_test = test_array[:,:-1], test_array[:,-1]
 
             models = {
-                "Random Forest": RandomForestRegressor(),
                 "Decision Tree": DecisionTreeRegressor(),
+                "Random Forest": RandomForestRegressor(),
                 "Gradient Boosting": XGBRegressor(),
                 "CatBoosting": CatBoostRegressor(verbose=False),
                 "Linear Regression": LinearRegression(),
                 "K-Neighbours": KNeighborsRegressor()
             }
+            params={
+                "Decision Tree": {
+                    'criterion':['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
+                    # 'splitter':['best','random'],
+                    # 'max_features':['sqrt','log2'],
+                },
+                "Random Forest":{
+                    # 'criterion':['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
+                    # 'max_features':['sqrt','log2',None],
+                    'n_estimators': [8,16,32,64,128,256]
+                },
+                "Gradient Boosting":{
+                    'learning_rate':[.1,.01,.05,.001],
+                    'n_estimators': [8,16,32,64,128,256]
+                },
+                "CatBoosting":{
+                    'depth':[6,8,10],
+                    'learning_rate':[.1,.01,.05,.001],
+                    'iterations':[30,50,100]
+                },
+                "Linear Regression":{},
+                "K-Neighbours":{}
+            }
 
-            model_report: dict = evaluate_model(X_train=X_train, y_train=y_train, X_test=X_test, y_test=y_test, models=models)
-
-           
+            model_report: dict = evaluate_model(X_train=X_train, y_train=y_train, X_test=X_test, y_test=y_test, models=models,params=params)
+            logging.info(f"Model Report: {model_report}")
             best_model_score = max(sorted(model_report.values()))
-
+            
            
             best_model_name = list(model_report.keys())[list(model_report.values()).index(best_model_score)]
 
--- src/utils.py
+++ src/utils.py
@@ -1,15 +1,13 @@
 import os
 import sys
+import pickle
+from sklearn.model_selection import GridSearchCV
 
 from src.exception import CustomException
-import pandas as pd
-import numpy as np
 from sklearn.metrics import r2_score
-
 
 def save_object(file_path, obj):   
     try:
-        import pickle
         dir_path = os.path.dirname(file_path)
         os.makedirs(dir_path, exist_ok=True)
         with open(file_path, 'wb') as file_obj:
@@ -17,17 +15,23 @@ def save_object(file_path, obj):
     except Exception as e:
         raise CustomException(e, sys)
     
-def evaluate_model(X_train, y_train, X_test, y_test, models):
+def evaluate_model(X_train, y_train, X_test, y_test, models, params):
     try:
         report = {}
         for i in range(len(models)):
-            model = list(models.values())[i]
+            model_name = list(models.keys())[i]
+            model = models[model_name]
+            param = params.get(model_name, {})
+            gs = GridSearchCV(estimator=model, param_grid=param, cv=3)
+            gs.fit(X_train, y_train)
+            if param:
+                model.set_params(**gs.best_params_)
             model.fit(X_train, y_train)
             y_train_pred = model.predict(X_train)
             y_test_pred = model.predict(X_test)
             train_model_score = r2_score(y_train, y_train_pred)
             test_model_score = r2_score(y_test, y_test_pred)
-            report[list(models.keys())[i]] = test_model_score
+            report[model_name] = test_model_score
         return report
     except Exception as e:
         raise CustomException(e, sys)
