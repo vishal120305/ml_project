1d53a9f5fbbe694407c512d96f2e823a2530d467
Vishal <vishal.dhasi2004@gmail.com>
2025-09-11 16:15:46 +0530

    html to json

--- application.py
+++ application.py
@@ -18,27 +18,39 @@ def python_version():
 @app.route('/')
 def home():
     return render_template('index.html')
-
 @app.route('/predict', methods=['GET', 'POST'])
 def predict_datapoint():
-    if request.method=='GET':
-        return render_template('home.html')
+    if request.method == 'GET':
+        return jsonify({'message': 'Send a POST request with student data'})
     else:
-        data=CustomData(
-            gender=request.form.get('gender'),
-            race_ethnicity=request.form.get('race_ethnicity'),
-            parental_level_of_education=request.form.get('parental_level_of_education'),
-            lunch=request.form.get('lunch'),
-            test_preparation_course=request.form.get('test_preparation_course'),
-            writing_score=request.form.get('writing_score'),
-            reading_score=request.form.get('reading_score')
-        )
-        pred_df=data.get_data_as_dataframe()
-        print(pred_df) 
-
-        predict_pipeline=PredictPipeline()
-        results=predict_pipeline.predict(pred_df)
-        return render_template('home.html', results=results[0])
+        try:
+            # Get JSON data from request
+            data = request.get_json()
+            
+            # Create CustomData object
+            student_data = CustomData(
+                gender=data.get('gender'),
+                race_ethnicity=data.get('race_ethnicity'),
+                parental_level_of_education=data.get('parental_level_of_education'),
+                lunch=data.get('lunch'),
+                test_preparation_course=data.get('test_preparation_course'),
+                writing_score=float(data.get('writing_score')),
+                reading_score=float(data.get('reading_score'))
+            )
+            
+            # Get data as DataFrame and predict
+            pred_df = student_data.get_data_as_dataframe()
+            predict_pipeline = PredictPipeline()
+            results = predict_pipeline.predict(pred_df)
+            
+            # Return JSON response
+            return jsonify({
+                'prediction': float(results[0]),
+                'status': 'success'
+            })
+            
+        except Exception as e:
+            return jsonify({'error': str(e), 'status': 'error'}), 400
         
     
 
